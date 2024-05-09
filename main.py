f62b5fd32a040d3249b6d4b874345af5a4f88e00
JeffHahn97 <jef.hahn97@gmail.com>
2024-05-09 13:20:36 +0200

    implemented letter count function

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    num_letters = get_letter_count(text)
     print(f"{num_words} words found in the document")
+    print(f"{num_letters} letters found in the document")
 
 
 def get_num_words(text):
@@ -17,6 +19,14 @@ def get_book_text(path):
 def get_letter_count(text):
     book_string =  text
     lowered_book_string = book_string.lower()
+    character_dict = {}
+    for char in lowered_book_string:
+        if char in character_dict:
+            character_dict[char] += 1
+        else:
+            character_dict[char] = 1
+    return character_dict
+    
     
 
 
